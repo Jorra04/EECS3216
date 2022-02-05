5592af42e4bef6da0714bc60a2fdf66420092768
unknown <jorra04@my.yorku.ca>
2022-02-04 23:50:55 -0500

    Fixed lab 3.

--- Lab 3/Lab3.v
+++ Lab 3/Lab3.v
@@ -1,82 +1,72 @@
-module Lab3(leftLights, rightLights, error, sw0, sw1, clk, rst);
+module Lab3(leftLights, rightLights, clk, rst, sw0, sw1, err);
 
-	output[2:0] leftLights, rightLights;
-	output[6:0] error;
-	input sw0, sw1, clk, rst;
 	
-	reg[31:0] counter = 0;
-	reg[2:0] leftLights_tmp = 0;
-	reg[2:0] rightLights_tmp = 0;
-	reg[6:0] tmp_err;
-	integer rightLightsIndex = 2;
-	integer leftLightsIndex = 0;
+	input clk, rst, sw0, sw1;
+	output[2:0] leftLights, rightLights;
+	output[6:0] err;
+	
+	reg[2:0] leftLights_tmp, rightLights_tmp;
+	reg[6:0] err_tmp;
+	reg[31:0] counter = 32'd0;
+	
+	integer index = 0;
+	integer flashCount = 0;
 	assign leftLights = leftLights_tmp;
 	assign rightLights = rightLights_tmp;
-	assign error = tmp_err;
-	
-	
-	
+	assign err = err_tmp;
 	always@(posedge clk or negedge rst) begin
-		
+	
 		if(~rst) begin
-			counter = 0;
-			rightLightsIndex = 2;
-			leftLightsIndex = 0;
-			tmp_err = 7'b1111111;
 			leftLights_tmp = 3'b000;
 			rightLights_tmp = 3'b000;
-			
-		end else begin		
-			
-			if((sw0 == 1) && (sw1 == 1)) begin //Both on, error
-				tmp_err = 7'b0110000;
+			index = 0;
+			counter = 0;
+			err_tmp = 7'b1111111;
+		end else begin
+			if(sw0 && sw1) begin
 				leftLights_tmp = 3'b000;
 				rightLights_tmp = 3'b000;
-				rightLightsIndex = 2;
-				leftLightsIndex = 0;
+				index = 0;
 				counter = 0;
-			end else if((sw0 == 1) && (sw1 == 0)) begin //Right Turn Signal
-				counter = counter + 1;
-				tmp_err = 7'b1111111;
-				leftLightsIndex = 0;
-
-				if(counter == 25000000) begin
-					counter = 0;
-					if(rightLightsIndex == -1) begin
-						rightLights_tmp = 3'b000;
-						rightLightsIndex = 2;
-					end else begin
-						rightLights_tmp[rightLightsIndex] = 1;
-						rightLightsIndex = rightLightsIndex - 1;
+				err_tmp = 7'b0000110;
+			end else begin
+				if(sw0) begin
+					err_tmp = 7'b1111111;
+					counter = counter + 1;
+					if(counter == 25000000) begin
+						counter = 0;
+						if(index > 2) begin
+							index = 0;
+							leftLights_tmp = 3'b000;
+							rightLights_tmp = 3'b000;
+						end else begin
+							rightLights_tmp[index] = 1;
+							index = index + 1;
+						end
 					end
-				end
-			end else if((sw0 == 0) && (sw1 == 1)) begin //Left Turn Signal
-				counter = counter + 1;
-				tmp_err = 7'b1111111;
-				rightLightsIndex = 2;
-				
-				if(counter == 25000000) begin
-					counter = 0;
-					if(leftLightsIndex == 3) begin
-						leftLights_tmp = 3'b000;
-						leftLightsIndex = 0;
-					end else begin
-						leftLights_tmp[leftLightsIndex] = 1;
-						leftLightsIndex = leftLightsIndex + 1;
+				end else if(sw1) begin
+					err_tmp = 7'b1111111;
+					counter = counter + 1;
+					if(counter == 25000000) begin
+						counter = 0;
+						if(index > 2) begin
+							index = 0;
+							leftLights_tmp = 3'b000;
+							rightLights_tmp = 3'b000;
+						end else begin
+							leftLights_tmp[index] = 1;
+							index = index + 1;
+						end
 					end
+				end else if(!sw0 && !sw1) begin
+					err_tmp = 7'b1111111;
+					leftLights_tmp = 3'b000;
+					rightLights_tmp = 3'b000;
+					index = 0;
+					counter = 0;
 				end
-			end else if((sw0 == 0) && (sw1 == 0)) begin //Both off
-				counter = 0;
-				rightLightsIndex = 2;
-				leftLightsIndex = 0;
-				tmp_err = 7'b1111111;
-				leftLights_tmp = 3'b000;
-				rightLights_tmp = 3'b000;
 			end
-		
 		end
-		
+	
 	end
-
-
 endmodule
