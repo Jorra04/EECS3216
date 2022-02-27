b0dc6dfee12808c9d71b9f6ed763625532fb7c63
unknown <jorra04@my.yorku.ca>
2022-02-27 14:58:17 -0500

    Finished lab 4 (mostly).

--- Lab 4/Lab4.sv
+++ Lab 4/Lab4.sv
@@ -3,95 +3,97 @@ module Lab4(sw0, sw1, sw2, clk, rst, seg0, seg1);
 	input sw0, sw1, sw2, clk, rst;
 	output[6:0] seg0, seg1;
 	reg[32:0] counter;
-	reg[3:0] value, value2, tmp, tmp2;
-	reg[6:0] currNum_tmp1, currentNum_tmp2, currentNum_tmp3, currentNum_tmp4 = 0;
+	reg[3:0] value, value2;
+	reg[6:0] currNum_tmp1 = 0;
 	
 	reg[6:0] acc = 0;
-	assign acc = currNum_tmp1 + currentNum_tmp2 + currentNum_tmp3 + currentNum_tmp4;
-
-	always_ff@(negedge rst, posedge sw0) begin
-		if(~rst) begin
-			currentNum_tmp2 <= 0;
-		end else begin
-		
-			if(sw0) begin
-				currentNum_tmp2 <= currentNum_tmp2 + 6'b000101;
-			end
-			
-		end
-	end
+	assign acc = currNum_tmp1;
 	
-	always_ff@(negedge rst, posedge sw1) begin
-		if(~rst) begin
-			currentNum_tmp3 <= 0;
-		end else begin
-		
-			if(sw1) begin
-				currentNum_tmp3 <= currentNum_tmp3 + 6'b001010;
-			end
-			
-		end
-	end
+	bit sw0_prev, sw1_prev, sw2_prev = 0;
 	
-	always_ff@(negedge rst, posedge sw2) begin
-		if(~rst) begin
-			currentNum_tmp4 <= 0;
-		end else begin
-		
-			if(sw2) begin
-				currentNum_tmp4 <= currentNum_tmp4 + 6'b010100;
-			end
-			
-		end
-	end
-	
+	reg[6:0] flashing = 0;
 
 	always_ff@(posedge clk, negedge rst) begin
 	
 		if(~rst) begin
 			counter <= 0;
 			currNum_tmp1 <= 0;
+			flashing <= 0;
 		end else begin
-			if(acc > 0) begin
+			
+			//Only count down if we are greater tha
+			if(acc >= 0) begin
 				counter <= counter + 1;
 				if(counter == 49999999) begin
 					counter <= 0;
-					currNum_tmp1 <= currNum_tmp1 - 1;
+					if(acc > 0 && !(!sw0_prev && sw0) && !(!sw1_prev && sw1) && !(!sw2_prev && sw2)) currNum_tmp1 <= currNum_tmp1 - 1;
+				
+					if( !(sw0_prev) && (sw0)) begin
+
+						currNum_tmp1 <= ((currNum_tmp1 + 5) > 99) ? 99 : (currNum_tmp1 + 5);
+						sw0_prev = 1;
+					end
+				
+					if(!(sw1_prev) && (sw1)) begin
+						currNum_tmp1 <= ((currNum_tmp1 + 10) > 99) ? 99 : (currNum_tmp1 + 10);
+						sw1_prev = 1;
+					end
+					
+					if(!(sw2_prev) && (sw2)) begin
+						currNum_tmp1 <= ((currNum_tmp1 + 20) > 99) ? 99 : (currNum_tmp1 + 20);
+						sw2_prev = 1;
+					end
+					
+					//Update prev states with the current states
+					sw0_prev <= sw0;
+					sw1_prev <= sw1;
+					sw2_prev <= sw2;
 				end
+				
+				if(counter == 12499999 && (acc <= 10 && acc > 0)) begin
+					
+					flashing <= ~flashing;
+				
+				end
+				
+				if(acc == 0) flashing <= 7'b0000000;
+				
 			end
+
 			
 			
 		end
 	end
-	assign value = acc % 10;
-	assign value2 = (acc /10) % 10;
+
+	assign value = (acc % 10) ;
+	assign value2 = ((acc /10) % 10) ;
 		
-	B2D hex01(value, seg0);
-	B2D hex02(value2, seg1);		
+	B2D hex01(value, seg0, flashing);
+	B2D hex02(value2, seg1,flashing);		
 		
 endmodule
 
 
-module B2D(inputBus, outputBus);
+module B2D(inputBus, outputBus, flashing);
 
 	input [4:0] inputBus;
+	input[6:0] flashing;
 	output [6:0] outputBus;
 	
-	
-	always@(inputBus) begin
+		always@(inputBus) begin
 		case(inputBus)
-        5'b00000: outputBus = 7'b1000000; //0
-        5'b00001: outputBus = 7'b1111001; //1
-        5'b00010: outputBus = 7'b0100100; //2
-        5'b00011: outputBus = 7'b0110000; //3
-        5'b00100: outputBus = 7'b0011001; //4
-        5'b00101: outputBus = 7'b0010010; //5
-        5'b00110: outputBus = 7'b0000010; //6
-        5'b00111: outputBus = 7'b1111000; //7
-        5'b01000: outputBus = 7'b0000000; //8
-        5'b01001: outputBus = 7'b0010000; //9
+        5'b00000: outputBus = 7'b1000000 | flashing; //0
+        5'b00001: outputBus = 7'b1111001| flashing; //1
+        5'b00010: outputBus = 7'b0100100| flashing; //2
+        5'b00011: outputBus = 7'b0110000| flashing; //3
+        5'b00100: outputBus = 7'b0011001| flashing; //4
+        5'b00101: outputBus = 7'b0010010| flashing; //5
+        5'b00110: outputBus = 7'b0000010| flashing; //6
+        5'b00111: outputBus = 7'b1111000| flashing; //7
+        5'b01000: outputBus = 7'b0000000| flashing; //8
+        5'b01001: outputBus = 7'b0010000| flashing; //9
 		 endcase
-		end
 	
-
+	end
 endmodule
+
