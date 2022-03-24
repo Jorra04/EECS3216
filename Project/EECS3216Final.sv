84c5b150909ae97f49056dcb26c8c0f3790bb30c
unknown <jorra04@my.yorku.ca>
2022-03-23 21:42:50 -0400

    Added some additional logic for the game, created and finished the game over screen.

--- Project/EECS3216Final.sv
+++ Project/EECS3216Final.sv
@@ -10,7 +10,7 @@ module EECS3216Final(clkin,rst,btn_up,hsync,vsync,r,g,b, tmp, seg0, seg1);
 	output[6:0] seg0, seg1;
 	
 	//timings variables
-	wire clk25M, clk1M, gravityClock, pipeClock,scoreClk;
+	wire clk25M, clk1M, gravityClock, pipeClock,scoreClk, gameOverAnimationClk;
 	reg de;
 	reg [9:0] x, y;
 	reg[9:0] tmp_playerY, tmp2_playerY = 0;
@@ -21,19 +21,19 @@ module EECS3216Final(clkin,rst,btn_up,hsync,vsync,r,g,b, tmp, seg0, seg1);
 	
 	reg[7:0] score, lastScore;
 	reg[6:0] flashing = 0;
+	reg[3:0] flashText = 0;
 	reg[3:0] onesPlace, tensPlace;
 	bit gameStarted = 0;
 	bit gameOver = 0;
 	
 	wire[2:0] rng_out;
 	
-	reg[8:0] pipe_opening_top_arr[6:0] = 		'{9'd250, 3'd150,3'd50,3'd20,3'd70,3'd300,3'd400};
-	reg[8:0] pipe_opening_bottom_arr [6:0] = 	'{9'd450, 9'd350,9'd250,3'd220,3'd270,3'd500,3'd600};
+	reg[8:0] pipe_opening_top_arr[6:0] = 		'{9'd250, 9'd150,9'd50,9'd20,9'd70,9'd300,9'd380};
 	
-	reg[8:0] pipe_openning_top, pipe_opening_bottom;
+	
+	reg[8:0] pipe_openning_top;
 	
 	assign pipe_openning_top = pipe_opening_top_arr[rng_out];
-	assign pipe_opening_bottom = pipe_opening_bottom_arr[rng_out];
 	
 	bit addScore = 0;
 	bit score5Achieved, score15Achieved,score30Achieved = 0;
@@ -42,13 +42,12 @@ module EECS3216Final(clkin,rst,btn_up,hsync,vsync,r,g,b, tmp, seg0, seg1);
 	bit playerOutOfBounds = 0;
 	bit playerHitPipe = 0;
 	bit overflow = 0;
-	bit powerUp = 0;
-	bit powerUpFlashing = 0;
+	
 	assign gameOver = (playerOutOfBounds | playerHitPipe);
 	
 	assign playery = tmp_playerY + tmp2_playerY;
 	assign playerx = tmp_playerX;
-	reg[3:0] pipe_clock_divisor = 0;
+	reg[3:0] pipe_clock_divisor = 1;
 	output tmp;
 //
 pll	pll_inst (
@@ -63,6 +62,11 @@ pll	pll_inst (
 	gravityClockDivder gravityDivider (
   .clock_in(clkin), 
   .clock_out(gravityClock)
+ );
+ 
+ gameOverAnimationClockDivider gameOverclkdiv (
+  .clock_in(clkin), 
+  .clock_out(gameOverAnimationClk)
  );
  
  pipeClockDivider pipeDivider (
@@ -103,6 +107,44 @@ pll	pll_inst (
 	parameter SAFE_AREA = 150;
 	parameter PIPE_WIDTH = 64;
 	
+	
+	
+	parameter BLOCK_SIZE = 16;
+	parameter LETTER_SPACING = 70;
+	
+	parameter TOP_BLOCK_1_START = 160;
+	parameter TOP_BLOCK_1_END = TOP_BLOCK_1_START +LETTER_SPACING;
+	
+	parameter TOP_BLOCK_2_START = TOP_BLOCK_1_END + LETTER_SPACING;
+	parameter TOP_BLOCK_2_END = TOP_BLOCK_2_START +LETTER_SPACING;
+	
+	parameter TOP_BLOCK_3_START = TOP_BLOCK_2_END + LETTER_SPACING;
+	parameter TOP_BLOCK_3_END = TOP_BLOCK_3_START +LETTER_SPACING;
+	
+	
+	//Bottom Letters
+	parameter BOTTOM_BLOCK_1_START = 84;
+	parameter BOTTOM_BLOCK_1_END = BOTTOM_BLOCK_1_START +LETTER_SPACING;
+	
+	parameter BOTTOM_BLOCK_2_START = BOTTOM_BLOCK_1_END + LETTER_SPACING;
+	parameter BOTTOM_BLOCK_2_END = BOTTOM_BLOCK_2_START +LETTER_SPACING;
+	
+	parameter BOTTOM_BLOCK_3_START = BOTTOM_BLOCK_2_END + LETTER_SPACING;
+	parameter BOTTOM_BLOCK_3_END = BOTTOM_BLOCK_3_START +LETTER_SPACING;
+	
+	parameter BOTTOM_BLOCK_4_START = BOTTOM_BLOCK_3_END + LETTER_SPACING;
+	parameter BOTTOM_BLOCK_4_END = BOTTOM_BLOCK_4_START +LETTER_SPACING;
+	
+	parameter TOP_LETTERS_START = 45;
+	parameter TOP_LETTERS_END = TOP_LETTERS_START + 75;
+
+	parameter BOTTOM_LETTERS_START = 295;
+	parameter BOTTOM_LETTERS_END = BOTTOM_LETTERS_START + 75;
+	
+	
+	reg[9:0] gameOverAnimationRoll = 0;
+	bit gameOverScreenBlack = 0;
+	
 	always_comb begin
 		hsync = ~(x >= HS_STA && x < HS_END);  
       vsync = ~(y >= VS_STA && y < VS_END);
@@ -115,51 +157,300 @@ pll	pll_inst (
 			x <= 0;
 			y <= 0;
 			playerHitPipe <= 0;
-			pipe_opening_top_arr <= 		'{9'd50, 9'd100,9'd150,9'd200,9'd250,9'd300,9'd350};
-			pipe_opening_bottom_arr <= 	'{9'd450, 9'd350,9'd250,3'd220,3'd270,3'd500,3'd600};
+			pipe_opening_top_arr <= 		'{9'd250, 9'd150,9'd50,9'd20,9'd70,9'd300,9'd380};
 			tmp <= 0;
 		end else begin 
-			if(~gameOver) begin
 				if(de)begin
-				if(((playerx + PLAYER_DIMENSIONS > pipeX) && (playerx + PLAYER_DIMENSIONS < pipeX + PIPE_WIDTH)) && 
-				(playery <= pipe_openning_top || (playery + PLAYER_DIMENSIONS) >= pipe_openning_top + SAFE_AREA)) begin
-						playerHitPipe <= 1 & !powerUp;
-				end if(x >= playerx && x <= playerx+PLAYER_DIMENSIONS && y >= playery && y <= playery+PLAYER_DIMENSIONS)begin
-					//bird is kind of yellow
-					r <= 4'b1111;
-					g <= 4'b1111;
-					b <= 4'b0000;
-				end else if(x >= pipeX && x <= pipeX+PIPE_WIDTH) begin
 					
-					if(y >= pipe_openning_top && y <= pipe_openning_top + SAFE_AREA) begin
+					if(~gameOver) begin
+						if((((playerx + PLAYER_DIMENSIONS > pipeX) && (playerx + PLAYER_DIMENSIONS < pipeX + PIPE_WIDTH)) || (((playerx > pipeX) && (playerx < pipeX + PIPE_WIDTH)) )) && 
+						(playery <= pipe_openning_top || (playery + PLAYER_DIMENSIONS) >= pipe_openning_top + SAFE_AREA)) begin
+						playerHitPipe <= 1;
+					end if(x >= playerx && x <= playerx+PLAYER_DIMENSIONS && y >= playery && y <= playery+PLAYER_DIMENSIONS)begin
+						r <= 4'b1111;
+						g <= 4'b1111;
+						b <= 4'b0000;
+					
+						
+					end else if(x >= pipeX && x <= pipeX+PIPE_WIDTH) begin
+					
+						if((y >= pipe_openning_top && y <= pipe_openning_top + SAFE_AREA)) begin
 						
 					
-					//blue background
+							//blue background
+							r <= 4'b1000;
+							g <= 4'b1100;
+							b <= 4'b1110;
+							
+						end 
+						else begin
+						
+							//Green pipes
+							r <= 4'b0011;
+							g <= 4'b1010;
+							b <= 4'b0010;
+					
+						end
+
+				
+					end else begin
+						//background is sky blue
+						
 						r <= 4'b1000;
 						g <= 4'b1100;
 						b <= 4'b1110;
-						
+					end
+					
 					end else begin
-						//Green pipes
-						r <= 4'b0011;
-						g <= 4'b1010;
-						b <= 4'b0010;
+						//Gameover
+						
+						if(~gameOverScreenBlack) begin
+						
+							if(((y <= gameOverAnimationRoll) && ((x > 0 && x < 120) || (x > 260 && x < 380) || (x > 520 && x < 640))) || 
+							((y >= VA_END-gameOverAnimationRoll) && ((x >= 120 && x <= 260) || (x >= 380 && x <= 520)))) begin
+								r <= 4'b0000;
+								g <= 4'b0000;
+								b <= 4'b0000;
+							end else begin
+							
+								r <= 4'b1111;
+								g <= 4'b0000;
+								b <= 4'b0000;
+								
+							end
+						
+						end else begin
+						
+							if(0 <= y && y < 250) begin
+								//Y
+								if((TOP_BLOCK_1_START <= x && x <= TOP_BLOCK_1_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END)) begin
+									if((TOP_BLOCK_1_START <= x && x <= TOP_BLOCK_1_START + BLOCK_SIZE) && (TOP_LETTERS_START <= y && y <= ((TOP_LETTERS_START  + TOP_LETTERS_END)/2 ))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_1_END - BLOCK_SIZE <= x && x <= TOP_BLOCK_1_END ) && (TOP_LETTERS_START <= y && y <= ((TOP_LETTERS_START  + TOP_LETTERS_END)/2 ))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_1_START <= x && x <= TOP_BLOCK_1_END) && (((TOP_LETTERS_START +TOP_LETTERS_END) /2) - (BLOCK_SIZE/2) <= y && 
+									y <= ((TOP_LETTERS_START +TOP_LETTERS_END) /2) + (BLOCK_SIZE /2 ))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									
+									end else if( (((TOP_BLOCK_1_START +TOP_BLOCK_1_END) /2) - (BLOCK_SIZE/2) <= x && x <= ((TOP_BLOCK_1_START +TOP_BLOCK_1_END) /2) + (BLOCK_SIZE /2 )) && 
+									(((TOP_LETTERS_START  + TOP_LETTERS_END)/2 ) <= y && y <= TOP_LETTERS_END ) ) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end
+									
+								//O
+								end else if((TOP_BLOCK_2_START <= x && x <= TOP_BLOCK_2_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END)) begin
+									if((TOP_BLOCK_2_START <= x && x <= TOP_BLOCK_2_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_START + BLOCK_SIZE)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_2_START <= x && x <= TOP_BLOCK_2_END) && (TOP_LETTERS_END - BLOCK_SIZE <= y && y <= TOP_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_2_START <= x && x <= TOP_BLOCK_2_START + BLOCK_SIZE) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_2_END - BLOCK_SIZE <= x && x <= TOP_BLOCK_2_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else  begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end
+									
+									
+								
+								end else if((TOP_BLOCK_3_START <= x && x <= TOP_BLOCK_3_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END)) begin
+									
+									if((TOP_BLOCK_3_START <= x && x <= TOP_BLOCK_3_END) && (TOP_LETTERS_END - BLOCK_SIZE <= y && y <= TOP_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_3_START <= x && x <= TOP_BLOCK_3_START + BLOCK_SIZE) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((TOP_BLOCK_3_END - BLOCK_SIZE <= x && x <= TOP_BLOCK_3_END) && (TOP_LETTERS_START <= y && y <= TOP_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else  begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end
+								
+								end else begin
+									r <= ~flashText;
+									g <= 4'b0000;
+									b <= 4'b0000;
+									
+								end
+								
+								
+								
+								
+							end else begin
+							
+								if((BOTTOM_BLOCK_1_START <= x && x <= BOTTOM_BLOCK_1_END) && (BOTTOM_LETTERS_START < y && y < BOTTOM_LETTERS_END)) begin
+									if((BOTTOM_BLOCK_1_START <= x && x <= BOTTOM_BLOCK_1_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((BOTTOM_BLOCK_1_START <= x && x <= BOTTOM_BLOCK_1_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end
+								
+								end else if((BOTTOM_BLOCK_2_START <= x && x <= BOTTOM_BLOCK_2_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END)) begin
+									
+									if((BOTTOM_BLOCK_2_START <= x && x <= BOTTOM_BLOCK_2_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_START + BLOCK_SIZE)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((BOTTOM_BLOCK_2_START <= x && x <= BOTTOM_BLOCK_2_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((BOTTOM_BLOCK_2_START <= x && x <= BOTTOM_BLOCK_2_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else if((BOTTOM_BLOCK_2_END - BLOCK_SIZE <= x && x <= BOTTOM_BLOCK_2_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END))begin
+
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end else  begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									
+									end
+								
+								end else if((BOTTOM_BLOCK_3_START <= x && x <= BOTTOM_BLOCK_3_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END)) begin
+									
+									if((BOTTOM_BLOCK_3_START <= x && x <= BOTTOM_BLOCK_3_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_START + BLOCK_SIZE)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_3_START <= x && x <= BOTTOM_BLOCK_3_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START <= y && y <= ((BOTTOM_LETTERS_START + BOTTOM_LETTERS_END)/2))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_3_END -  BLOCK_SIZE <= x && x <= BOTTOM_BLOCK_3_END ) && (((BOTTOM_LETTERS_START + BOTTOM_LETTERS_END)/2) <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_3_START <= x && x <= BOTTOM_BLOCK_3_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_3_START <= x && x <= BOTTOM_BLOCK_3_END) && (((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) - (BLOCK_SIZE/2) <= y && 
+										y <= ((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) + (BLOCK_SIZE /2 ))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end
+								
+								end else if((BOTTOM_BLOCK_4_START <= x && x <= BOTTOM_BLOCK_4_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END)) begin
+									
+									if((BOTTOM_BLOCK_4_START <= x && x <= BOTTOM_BLOCK_4_END) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_START + BLOCK_SIZE)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_4_START <= x && x <= BOTTOM_BLOCK_4_START + BLOCK_SIZE) && (BOTTOM_LETTERS_START <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_4_START <= x && x <= BOTTOM_BLOCK_4_END) && (BOTTOM_LETTERS_END - BLOCK_SIZE <= y && y <= BOTTOM_LETTERS_END)) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else if((BOTTOM_BLOCK_4_START <= x && x <= BOTTOM_BLOCK_4_END) && (((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) - (BLOCK_SIZE/2) <= y && 
+										y <= ((BOTTOM_LETTERS_START +BOTTOM_LETTERS_END) /2) + (BLOCK_SIZE /2 ))) begin
+										r <= flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end else begin
+										r <= ~flashText;
+										g <= 4'b0000;
+										b <= 4'b0000;
+									end
+								
+								end else begin
+								
+									r <= ~flashText;
+									g <= 4'b0000;
+									b <= 4'b0000;
+								end
+								
+							end
+
+						end
 					
 					end
-
+					
 				
-				end else begin
-					//background is sky blue
-					r <= 4'b1000;
-					g <= 4'b1100;
-					b <= 4'b1110;
+				end else begin	
+					r <= 4'b0000;
+					g <= 4'b0000;
+					b <= 4'b0000;
 				end
-				
-			end else begin		
-				r <= 4'b0000;
-				g <= 4'b0000;
-				b <= 4'b0000;
-			end
+			
+
+			
 			
 			//screen timing logic
 			if (x == LINE) begin  
@@ -167,13 +458,6 @@ pll	pll_inst (
 				y <= (y == SCREEN) ? 0 : y + 1;  
 			end else begin
 				x <= x + 1;
-			end
-			
-			end else begin
-				r <= 4'b0000;
-				g <= 4'b0000;
-				b <= 4'b0000;
-			
 			end
 				
 		end
@@ -216,7 +500,6 @@ pll	pll_inst (
 			
 		end else begin
 			if((btn_up&gameStarted) && ~gameOver) begin
-
 				if(playery + PLAYER_DIMENSIONS < VA_END && playery > 0) begin
 				
 					tmp2_playerY <= tmp2_playerY + 5;
@@ -226,8 +509,6 @@ pll	pll_inst (
 				end
 			end
 
-
-		
 	
 		end
 	
@@ -242,12 +523,10 @@ pll	pll_inst (
 			pipeX <= HA_END;
 			pipeY <= 0;
 			score <= 0;
-			pipe_clock_divisor <= 0;
+			pipe_clock_divisor <= 1;
 			score5Achieved <= 0;
 			score15Achieved <= 0;
 			score30Achieved <= 0;
-			powerUp <= 0;
-			powerUpFlashing <= 0;
 		end else begin
 			if(gameStarted && ~gameOver) begin
 				if((pipeX) <= 10) begin
@@ -292,11 +571,36 @@ pll	pll_inst (
 	
 		if(~rst) begin
 			flashing <= 0;
+			flashText <= 0;
 		end else begin
 		
 			if(gameOver) begin
 				flashing <= ~flashing;
-					
+				flashText <= ~flashText;
+
+			end
+		
+		end
+	
+	end
+	
+	
+	always_ff@(posedge gameOverAnimationClk or negedge rst) begin
+		
+		if(~rst) begin
+			gameOverAnimationRoll <= 0;
+			gameOverScreenBlack <= 0;
+		end else begin
+		
+			if(gameOver) begin
+			
+				if(gameOverAnimationRoll + 5 < VA_END) begin
+					gameOverAnimationRoll <= gameOverAnimationRoll + 5;
+				end else begin
+					gameOverAnimationRoll <= VA_END;
+					gameOverScreenBlack <= 1;
+				end
+			
 			end
 		
 		end
